06898248a5db11c4b470b0703e4ac6b322758db4
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-04-21 07:08:12 +0530

    create the navbar component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,13 +7,15 @@ import DarkModeSwitch from "./DarkModeSwitch";
 export default function Header() {
   return (
     <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
-        <div className="flex gap-4">
+        <div className="flex gap-12">
             <MenuItem title="home" address="/" Icon={AiFillHome} />
             <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
         </div>
         <div className="flex items-center gap-4">
+            <div>
             <DarkModeSwitch/>
-        <Link href={"/"} className="flex gap-1 items-center">
+            </div>
+        <Link href={"/"} className="flex gap-2 items-center">
             <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">IMDb</span>
             <span className="text-xl hidden sm:inline">Clone</span>
         </Link>
